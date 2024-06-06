fcf689b21ede80bcbf2ce3470f7e8199269e7ead
zhwang <zhwang2012@yean.net>
2024-06-06 09:00:01 +0800

    修改数据库类型枚举实现

--- src/main/java/com/spms/common/enums/DatabaseTypeEnum.java
+++ src/main/java/com/spms/common/enums/DatabaseTypeEnum.java
@@ -1,15 +1,38 @@
 package com.spms.common.enums;
 
+import lombok.Getter;
+
 /**
  * @author zzypersonally@gmail.com
  * @description
  * @since 2024/5/21 15:34
  */
+@Getter
 public enum DatabaseTypeEnum {
-    Oracle,
-    SQLServer,
-    MySQL,
-    PostgresSQL,
-    ClickHouse
-    ;
+    Oracle("0", "Oracle"),
+    SQLServer("1", "SQLServer"),
+    MySQL("2", "MySql"),
+    PostgresSQL("3", "PostgresSQL"),
+    ClickHouse("6", "ClickHouse");
+
+    private String code;
+    private String name;
+
+    DatabaseTypeEnum(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public static String getNameByCode(String code) {
+        DatabaseTypeEnum[] var1 = values();
+
+        for (DatabaseTypeEnum e : var1) {
+            if (code.equals(e.getCode())) {
+                return e.getName();
+            }
+        }
+
+        return null;
+    }
+
 }
